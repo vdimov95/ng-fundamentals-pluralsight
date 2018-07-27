0cb3c9ee92cb075bbaee302b499d6298ffc8458a
Vladimir Dimov <v.dimov@blubito.com>
2018-07-27 14:50:21 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
